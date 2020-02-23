c9f47213f0208e2f6bd39ff008949aba5ce54d8d
MaaElattar <maaelattar@gmail.com>
2020-02-23 22:20:49 +0200

    Add header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from '../core/services/user.service';
+import { User } from '../core/models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  currentUser: User;
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
+    this.subscription = this.userService.currentUser.subscribe(
+      userData => (this.currentUser = userData)
+    );
   }
 
+  onLogout() {
+    this.userService.purgeAuth();
+    this.router.navigateByUrl('/');
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
